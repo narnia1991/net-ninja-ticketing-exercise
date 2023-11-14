bdf2c4f5f250eadb85f3600cd17b71f8530c0b14
Narnia 1991 <narnia1991@proton.me>
2023-11-14 13:39:12 +0800

    Fix build

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const server = jsonServer.create()
 // Uncomment to allow write operations
 const fs = require('fs')
 const path = require('path')
-const filePath = path.join('./db.json')
+const filePath = path.join('api/db.json')
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db)
@@ -14,10 +14,6 @@ const router = jsonServer.router(db)
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-}))
 
 server.use(router)
 server.listen(4000, () => {
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
+    <main>
       <div className="p-8">
         <h3 className="py-8">
           This is an exercise of using Next 13 with app directory thanks to <Link className="text-slate-300" target="_blank" rel="noreferrer nofollow noopener" href="https://www.youtube.com/watch?v=TJQbDPGzm0Y&list=PL4cUxeGkcC9jZIVqmy_QhfQdi6mzQvJnT">
-            Net Ninja's Playlist
+            Net Ninja`&apos;`s Playlist
           </Link>
         </h3>
         <Link href={"/tickets"}>
